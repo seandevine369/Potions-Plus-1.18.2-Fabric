f3b3a1e2c4475fb8a1173556774dc7e4aa727a4b
seandevine369 <sdevine.ca@gmail.com>
2022-04-15 22:56:41 -0400

    First commit

---
+++ src/main/java/net/devity/potionsplus/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.devity.potionsplus.mixin;
+
+import net.devity.potionsplus.PotionsPlus;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		PotionsPlus.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
